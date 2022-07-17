65009a63918402e5ed2733dfbaec792d2ee64e57
redwine <rewindmin@naver.com>
2022-07-17 20:37:02 +0900

    20220717

---
+++ photo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name = 'photo'
+
+urlpatterns =[
+    path('', photo_list, name='photo_list'),
+]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Photo
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos': photos})
+
+
+
+
+
